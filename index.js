67deef235e25e033ca8d31799907bfdf3a4483f3
Subhojit0012 <subhojitroy00012@gmail.com>
2024-11-30 19:09:11 +0530

    commit

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import userRoute from "./routers/user.routes.js";
+import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("<p>Hello from server</p>");
