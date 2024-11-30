44176f79bd68d7993119ad7231e9bd38778a5580
Subhojit0012 <subhojitroy00012@gmail.com>
2024-12-01 00:06:52 +0530

    commit

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import "dotenv/config";
 import cors from "cors";
 import userRoute from "./routers/user.routes.js";
 import cookieParser from "cookie-parser";
+import morgan from "morgan";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
